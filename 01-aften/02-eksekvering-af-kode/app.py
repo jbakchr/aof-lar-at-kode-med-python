3598febad37d0e5c0900e579361a1486d47619e1
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-03-04 05:53:46 +0100

    initial commit

---
+++ 01-aften/02-eksekvering-af-kode/app.py
@@ -0,0 +1,15 @@
+"""
+Print "juletræ" figur for at vise ekskvering af kode linje for linje
+"""
+# print("  *")
+# print(" ***")
+# print("*****")
+# print("  *")
+
+"""
+Ændre rækkefølgen af statements for at vise ændringen i output
+"""
+# print("*****")
+# print("  *")
+# print(" ***")
+# print("  *")
