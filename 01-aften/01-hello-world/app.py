2ad0153a80e7072e7e6474dca90ecb6cb41f60a6
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-03-04 05:59:18 +0100

    Added print statements

--- 01-aften/01-hello-world/app.py
+++ 01-aften/01-hello-world/app.py
@@ -1,1 +1,10 @@
-# print 'Hello, World' her
+"""
+print 'Hello, World' her
+"""
+# print("Hello, World!")
+
+
+"""
+print hvad du vil - dit navn fx
+"""
+# print("Mit navn er Jonas Bak Phillipson - og jeg er super awesome")
