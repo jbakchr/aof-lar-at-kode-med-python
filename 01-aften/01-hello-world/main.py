9286a19e4e7493af64c6f31182bda4718f96bf45
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-09-09 13:23:02 +0200

    Changed some stuff

--- 01-aften/01-hello-world/main.py
+++ 01-aften/01-hello-world/main.py
@@ -1,17 +1,10 @@
 """
 print 'Hello, World' her ..
 """
-# print("Hello, World!")
+print("Hello, World!")
 
 
 """
 print hvad du vil - dit navn fx
 """
 # print("My name is Jonas Bak Phillipson - and I am super duper awesome!")
-
-"""
-Print flere ting på én gang .. OMG!
-"""
-# print("*** MIN HUSKESEDDEL **")
-# print("Køb ost!")
-# print("Ring til mor ..")
