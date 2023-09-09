ce64077ecfb1f81182d8f29c3c7a413a04cacec3
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-09-09 10:00:57 +0200

    Added stuff about variables

---
+++ 01-aften/03-variable/app.py
@@ -0,0 +1,38 @@
+"""
+print af program uden brug af variable
+"""
+# print("There once was a man named George, ")
+# print("he was 70 years old.")
+# print("He really liked the name George, ")
+# print("but he didn't like being 70 years old..")
+
+
+"""
+ændring af mandens navn og alder i programmet uden variable
+"""
+# print("There once was a man named Peter, ")
+# print("he was 152 years old.")
+# print("He really liked the name Peter, ")
+# print("but he didn't like being 152 years old..")
+
+"""
+print af program med brug af variable
+"""
+# name = "Jonas"
+# age = "90"
+
+# print("There once was a man named " + name + ", ")
+# print("he was " + age + " years old.")
+# print("He really liked the name " + name + ", ")
+# print("but he didn't like being " + age + " years old..")
+
+"""
+konvention for variabel navne
+"""
+# character_name = "Jonas"
+# character_age = "90"
+
+# print("There once was a man named Peter, ")
+# print("he was 152 years old.")
+# print("He really liked the name Peter, ")
+# print("but he didn't like being 152 years old..")
