fac4ef19d1b803ca836eaca57c507e95b9b02ccb
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-09-20 09:15:26 +0200

    Added stuff

--- 01-aften/01-hello-world/exercise1.py
+++
@@ -1,4 +0,0 @@
-""" 
-EXERCISE 1:
-Print the classic "Hello, World!" to the console - if it works you're now both a 'hacker' and a 'programming GOD'! 
-"""
--- 01-aften/01-hello-world/main.py
+++ 01-aften/01-hello-world/main.py
@@ -1,10 +1,4 @@
 """
-print 'Hello, World' her ..
+print 'Hello, World' and you're now .. a super duper awesome 'hacker' that can retire and live a lif of luxury (perhaps)
 """
 print("Hello, World!")
-
-
-"""
-print hvad du vil - dit navn fx
-"""
-# print("My name is Jonas Bak Phillipson - and I am super duper awesome!")
