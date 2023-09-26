d1afe071c77c4b395f6c66e9fadf76b45a73ae8e
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-09-26 03:18:11 +0200

    Added recap code

--- 01-lectures/02-aften/01-recap/main.py
+++ 01-lectures/02-aften/01-recap/main.py
@@ -1,10 +1,14 @@
 """
 Recap from first night lecture:
 
-- 1) Print something with the "print" function
+- 1) Print something with the "print" function - use both double and single quotes
 - 2) Clear the terminal
 - 3) Break your code, run it again and try to understand the error message (Google it!)
 - 4) Fix your code
 - 5) Learn to look up stuff in the Python documentation https://www.python.org/
 """
-print("Hello world!")
+print("Hello world in double quotes!")
+
+print('Single quotes work too')
+
+print("Hej", "med", "dig", sep="-", end="-ost")
