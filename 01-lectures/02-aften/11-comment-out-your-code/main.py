f8fd7086cbd2c0d0f5fed5fa56e6bb5cb648a262
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-09-26 06:26:33 +0200

    Added more comment stuff

---
+++ 01-lectures/02-aften/11-comment-out-your-code/main.py
@@ -0,0 +1,7 @@
+"""
+Exercise 11 - comment out code
+"""
+print("I will run!")
+print("When you use the # sign before a statement that line will not be executed")
+# print("I won't get printed ..")
+# print("Me neither!")
---
+++ 01-lectures/02-aften/12-multiline-comments/main.py
@@ -0,0 +1,7 @@
+"""
+This entire file won't print anything
+"""
+# print("OMG .. I'm commented out by the # sign!")
+# print("Me too ..")
+# print("What's a statement to do?")
+# print("Our li.. code has no meaning ")
