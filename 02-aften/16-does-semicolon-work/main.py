64d1d8759ee6e1acb49144fb74a1d4e4d4ab6690
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-09-26 02:39:23 +0200

    Moved stuff

--- 02-aften/16-does-semicolon-work/main.py
+++ 02-aften/16-does-semicolon-work/main.py
@@ -1,4 +1,4 @@
 """
 Insert semicolon after print statement .. does it work? And if so, is it the correct way of programming in Python?
 """
-print("Jonas' sønner elsker ost ..")
+print("Jonas' sønner elsker ost ..");
