b17b7e5e10972dfc36f792fefd021ef259e07910
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-09-28 16:08:11 +0200

    Added exercise

---
+++ 02-exercises/02-exercise/main.py
@@ -0,0 +1,9 @@
+"""
+EXERCISE
+Create a "multiply function that takes in 2 integers and which prints the result to the console
+
+HINT
+To complete this exercise you'll need to know about:
+- How to define a function in Python
+- How to use the multiply operator
+"""
---
+++ 02-exercises/02-exercise/solution/main.py
@@ -0,0 +1,13 @@
+"""
+EXERCISE
+Create a "multiply function that takes in 2 integers and which prints the result to the console
+
+HINT
+To complete this exercise you'll need to know about:
+- How to define a function in Python
+- How to use the multiply operator
+"""
+def multiply(num1, num2):
+  print(num1 * num2)
+
+multiply(5, 2)
