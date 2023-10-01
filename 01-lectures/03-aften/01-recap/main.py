7a25831f9431cf9fe688538604fae0475d7ff863
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-01 17:49:24 +0200

    Added more on data types

---
+++ 01-lectures/03-aften/01-recap/main.py
@@ -0,0 +1,33 @@
+"""
+Quick recap of:
+- 1) Indentation
+- 2) Comments
+- 3) 
+"""
+
+# Example 1: identation
+if True:
+  print("I'm indented!")
+
+# Example 2: Comments
+print("This works!") # This comment works
+# print("This won't work")
+
+# Example 3: Various data types
+
+# "str" - i.e. "String"
+name = "Jonas"
+
+# "int" - i.e. "Integer"
+age = 41
+
+# "bool" - i.e. "Boolean" (True or False values)
+your_awesome = True
+
+# "list"
+groceries = ["Apple", "Banana", "Bear"]
+
+# "dict" - i.e. "Dictonary" (key/value pair)
+my_data = {"name": "Jonas", "age": 41}
+
+# and so on and so on ..
---
+++ 01-lectures/03-aften/02-more-on-data-types/main.py
@@ -0,0 +1,21 @@
+"""
+Various examples of the constructor functions from different data types
+"""
+
+# strings
+name = str("Jonas")
+
+# integer
+age = int("10")
+
+# float
+bank_account = float("10.5")
+
+# boolean
+you_are_awesome = bool(True)
+
+# list
+lst = list(("apple", "banana", "beer"))
+
+# set
+groceries = set(("apple", "banana", "apple"))
