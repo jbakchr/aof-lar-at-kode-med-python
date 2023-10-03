ab6eb425b444e11f782c40a09aee2da23dce0a4d
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-03 20:27:53 +0200

    Added exercise 4

---
+++ 02-exercises/03-aften/04-exercise/main.py
@@ -0,0 +1,5 @@
+"""
+EXERCISE:
+
+Create a variable with a multiline string and print it to the terminal/console
+"""
---
+++ 02-exercises/03-aften/04-exercise/solution/main.py
@@ -0,0 +1,12 @@
+"""
+EXERCISE:
+
+Create a variable with a multiline string and print it to the terminal/console
+"""
+
+multiline_text = """Being part
+of this course
+is so freaking
+AWESOME!
+"""
+print(multiline_text)
