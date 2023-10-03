b5e4c009a1996a656c69fe94a919acf5edce385e
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-04 00:28:58 +0200

    Added 10 exercise

---
+++ 02-exercises/03-aften/10-exercise/main.py
@@ -0,0 +1,7 @@
+"""
+EXERCISE:
+
+Steps to complete exercise:
+- 1) Create two strings variables
+- 2) Concatenate the variables and print the result
+"""
---
+++ 02-exercises/03-aften/10-exercise/solution/main.py
@@ -0,0 +1,12 @@
+"""
+EXERCISE:
+
+Steps to complete exercise:
+- 1) Create two strings variables
+- 2) Concatenate the variables and print the result
+"""
+
+first_name = "Jonas"
+last_name = " Phillipson"
+
+print(first_name + last_name)
