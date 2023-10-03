5cba1c122170558f0cb7fa0d12908e1c98e89a77
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-04 00:16:54 +0200

    Added 09 exercise

---
+++ 02-exercises/03-aften/09-exercise/main.py
@@ -0,0 +1,8 @@
+"""
+EXERCISE:
+
+Complete this exercise by:
+- 1) Create a variable with your full name
+- 2) print your full name is uppercase
+- 3) split your full name into a list and print it
+"""
---
+++ 02-exercises/03-aften/09-exercise/solution/main.py
@@ -0,0 +1,13 @@
+"""
+EXERCISE:
+
+Complete this exercise by:
+- 1) Create a variable with your full name
+- 2) print your full name is uppercase
+- 3) split your full name into a list and print it
+"""
+
+full_name = "Jonas Bak Phillipson"
+
+print(full_name.upper())
+print(full_name.split(" "))
