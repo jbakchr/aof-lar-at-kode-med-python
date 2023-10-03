e8cc5f56c82e341f5455012725cdc82fbbb7641a
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-03 22:54:30 +0200

    Added string slicing

---
+++ 01-lectures/03-aften/05-more-on-strings/main.py
@@ -0,0 +1,22 @@
+"""
+Examples of slicing a string
+"""
+
+# Example 1 - String slicing with start and stop value
+ex1 = "Hello, World!"
+print(ex1[0:5])
+
+
+# Example 2 - String slicing only with stop value
+ex2 = "Awesome sauce"
+print(ex2[0:7])
+
+
+# Example 3 - String slicing only with start value
+ex3 = "Slice me!"
+print(ex3[6:])
+
+
+# Example 4 - String slicing with negative start and stop values
+ex4 = "We are the knights who say Ni ..."
+print(ex4[-6:-1])
