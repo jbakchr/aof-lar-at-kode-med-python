2c168979904c315339d67aad696d105f983a1090
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-03 18:44:07 +0200

    Added exercise 3

---
+++ 02-exercises/03-aften/03-exercise/main.py
@@ -0,0 +1,13 @@
+"""
+EXERCISE 3:
+
+Steps to complete exercise
+- 1) Create an integer variable called "age" and set it to some integer value
+- 2) Use an "if" statement to check whether the variable is in fact an integer
+- 3) print something if it is
+"""
+
+age = 40
+
+if type(age) == int:
+  print("Hurra!")
