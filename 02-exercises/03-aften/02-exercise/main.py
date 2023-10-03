1c59b88f762ffc6bda47c14b9e14b5d7bccfa27c
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-03 18:37:17 +0200

    Added exercise 2

---
+++ 02-exercises/03-aften/02-exercise/main.py
@@ -0,0 +1,5 @@
+"""
+EXERCISE 2:
+
+Use the print function to print the type of a variable of your liking
+"""
---
+++ 02-exercises/03-aften/02-exercise/solution/main.py
@@ -0,0 +1,7 @@
+"""
+SOLUTION TO EXERCISE 2:
+
+Use the print function to print the type of a variable of your liking
+"""
+name = "Jonas"
+print(type(name))
