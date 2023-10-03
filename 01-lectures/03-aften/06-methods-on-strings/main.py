08b11b2a53bf740d00df945e49465dbc76c7f683
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-03 23:43:57 +0200

    Added more string examples

---
+++ 01-lectures/03-aften/06-methods-on-strings/main.py
@@ -0,0 +1,17 @@
+"""
+Examples of string methods
+"""
+
+# Example 1 - making strings uppercase
+upper_txt = "Make me uppercase!"
+print(upper_txt.upper())
+
+
+# Example 2 - strip string for whitespaces
+strip_me = "  Please fix me   "
+print(strip_me.strip())
+
+
+# Example 3 - split string into list elements
+grocery_list = "Apples,Bananas,Beers"
+print(grocery_list.split(","))
---
+++ 01-lectures/03-aften/07-string-concatenation/main.py
@@ -0,0 +1,14 @@
+"""
+Examples of string concatenation
+"""
+
+# Example 1 - string concatenation of first name and last name that includes a space at the beginning
+first_name = "Jonas"
+last_name = " Phillipson"
+print(first_name + last_name)
+
+
+# Example 2 - string concatenation with manual insertion of with colon and whitespace
+title = "Benovelent Dictator for life"
+man = "Guido van Rossum"
+print(title + ": " + man)
