d7a35677cc0056e7c02ff9bbd2f4cbd79227dc81
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-03 17:49:23 +0200

    Added type function example

---
+++ 01-lectures/03-aften/03-type-function/main.py
@@ -0,0 +1,20 @@
+"""
+Various examples of using the "type" function
+"""
+
+# print type of a string
+name = "Jonas"
+print(type(name))
+
+# print type of an integer
+age = 41
+print(type(age))
+
+# print type of a list
+groceries = list(["apple", "banana", "beer"])
+print(type(groceries))
+
+
+# check if the variable "name" is a string
+if name == str:
+  print("Whoooa .. this variable is indeed a string!")
