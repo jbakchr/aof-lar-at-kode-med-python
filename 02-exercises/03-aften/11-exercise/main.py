7420be8d0df53e7583859e49146f335141275c4b
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-04 06:05:54 +0200

    Added 11 exercise

---
+++ 02-exercises/03-aften/11-exercise/main.py
@@ -0,0 +1,8 @@
+"""
+EXERCISE
+
+Steps to complete exercise:
+- 1) Create a variable called "first_name" initialized with a string containing your first name
+- 2) Create a variable cvalled "last_name" initialized with a string containing your last name
+- 3) Print your full name using the "f-string" format
+"""
---
+++ 02-exercises/03-aften/11-exercise/solution/main.py
@@ -0,0 +1,13 @@
+"""
+EXERCISE
+
+Steps to complete exercise:
+- 1) Create a variable called "first_name" initialized with a string containing your first name
+- 2) Create a variable called "last_name" initialized with a string containing your last name
+- 3) Print a sentence using the "f-string" format that starts with "Hi, my name is " followed by your full name
+"""
+
+first_name = "Jonas"
+last_name = "Phillipson"
+
+print(f"Hi, my name is {first_name} {last_name}")
