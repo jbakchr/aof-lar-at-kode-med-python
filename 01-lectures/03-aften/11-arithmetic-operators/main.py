f4995558e4c7eca60d8c507ce296c2f61330f969
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-05 09:20:58 +0200

    Added more examples

---
+++ 01-lectures/03-aften/11-arithmetic-operators/main.py
@@ -0,0 +1,15 @@
+"""
+Examples of arithmetic operators in Python
+"""
+
+num1 = 20
+num2 = 3
+
+print(num1 + num2)  # Addition
+print(num1 - num2)  # Subtraction
+print(num1 * num2)  # Multiplication
+print(num1 / num2)  # Division
+print(num1 % num2)  # Modulus
+print(num1 ** num2) # Exponential
+print(num1 // num2) # Floor division
+
