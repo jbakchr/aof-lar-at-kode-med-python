ce3530ab11e7db47c10031e11adacf53c440b4b5
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-05 13:11:52 +0200

    Added exercise 16

---
+++ 02-exercises/03-aften/16-exercise/main.py
@@ -0,0 +1,9 @@
+"""
+EXERCISE
+
+In this exercise you are to:
+- 1) Create a variable containing an empty list
+- 2) Add 3 elements to list
+- 3) Loop through the list printing each element
+"""
+
---
+++ 02-exercises/03-aften/16-exercise/solution/main.py
@@ -0,0 +1,20 @@
+"""
+SOLUTION TO EXERCISE
+
+In this exercise you are to:
+- 1) Create a variable called "groceries" containing an empty list
+- 2) Add 3 elements to list
+- 3) Loop through the list printing each element
+"""
+
+# Create empty list
+groceries = []
+
+# Add 3 elements to the list
+groceries.append("Carlsberg")
+groceries.append("Tuborg")
+groceries.append("Heineken")
+
+# Loop through list
+for el in groceries:
+  print(el)
