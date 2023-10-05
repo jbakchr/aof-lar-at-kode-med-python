c09331d687eae09acb8cc0f95aec666c7b087cd6
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-05 10:23:42 +0200

    Added assignment operator examples

--- 01-lectures/03-aften/12-assignment-operators/main.py
+++ 01-lectures/03-aften/12-assignment-operators/main.py
@@ -1,3 +1,26 @@
 """
-
+Examples of assignment operators
 """
+
+num1 = 20
+num2 = 3
+
+
+# Example 1 - Addition - num1 becomes 23
+num1 += num2
+print(num1)
+
+
+# Example 2 - Subtraction - num1 becomes 20 again
+num1 -= num2
+print(num1)
+
+
+# Example 3 - Multiplication - num1 now becomes 60
+num1 *= num2
+print(num1)
+
+
+# Example 4 - Division - num1 becomes 20 again
+num1 /= num2
+print(num1)
