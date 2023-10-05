3619d15d56bfd2b556e2b798086429e239267cf0
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-05 11:34:20 +0200

    Added exercise 15

---
+++ 02-exercises/03-aften/15-exercise/main.py
@@ -0,0 +1,6 @@
+"""
+EXERCISE
+
+Create two integer variables called "num1" and "num2" and assign them an integer value.
+Play around with the various assignment operators and print the result of these assigments
+"""
---
+++ 02-exercises/03-aften/15-exercise/solution/main.py
@@ -0,0 +1,21 @@
+"""
+EXERCISE
+
+Create two integer variables called "num1" and "num2" and assign them an integer value.
+Play around with the various assignment operators and print the result of these assigments
+"""
+
+num1 = 20
+num2 = 3
+
+num1 += num2
+print(num1)
+
+num1 -= num2
+print(num1)
+
+num1 *= num2
+print(num1)
+
+num1 /= num2
+print(num1)
