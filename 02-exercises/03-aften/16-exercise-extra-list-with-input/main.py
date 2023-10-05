ea96cf1e26bd563ca5ed2f0704578d742874d95d
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-05 13:29:33 +0200

    Added extra exercise 16

---
+++ 02-exercises/03-aften/16-exercise-extra-list-with-input/main.py
@@ -0,0 +1,17 @@
+"""
+EXTRA EXERCISE
+
+In this exercise you are to create an empty shopping list, add 3 items to by 
+use of the "input" function that we haven't used before and lastly print
+each item in the shopping list to the terminal
+
+To complete the exercise you should therefore follow these steps:
+
+- 1) Create a variable called "shopping_list" and assign it an empty list
+
+- 2) Use the range function to create a loop that'll do something 3 times
+- 3) Within this loop create a variable called "item" and assign it result of an input function
+- 4) Append the "item" variable to the "shopping_list"
+
+- 5) Loop through the shopping list printing one element on each iteration
+"""
