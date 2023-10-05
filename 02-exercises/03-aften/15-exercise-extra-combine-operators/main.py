96164e198918577741683db725397084dc41654e
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-05 11:52:25 +0200

    Added extra exercise 15

---
+++ 02-exercises/03-aften/15-exercise-extra-combine-operators/main.py
@@ -0,0 +1,11 @@
+"""
+EXTRA EXERCISE
+
+Given that we now know how to use arithmetic and assignment operators in Python you
+now be able to combine both of these in your code.
+
+In this exercise you are, therefore, to:
+- 1) Create 2 integer variables with numbers of your liking
+- 2) Assign the multiplication of these two variables and add the result to the first variable
+- 3) Print the value of your first variable
+"""
---
+++ 02-exercises/03-aften/15-exercise-extra-combine-operators/solution/main.py
@@ -0,0 +1,17 @@
+"""
+EXTRA EXERCISE
+
+Given that we now know how to use arithmetic and assignment operators in Python you
+now be able to combine both of these in your code.
+
+In this exercise you are, therefore, to:
+- 1) Create 2 integer variables with numbers of your liking
+- 2) Assign the multiplication of these two variables and add the result to the first variable
+- 3) Print the value of your first variable
+"""
+
+num1 = 10
+num2 = 20
+
+num1 += num1 * num2
+print(num1)
