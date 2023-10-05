579c31a29cbf26e6d2d24e7a728de135d4281e1b
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-05 10:31:04 +0200

    Added exercise 14

---
+++ 02-exercises/03-aften/14-exercise/solution/main.py
@@ -0,0 +1,17 @@
+"""
+EXERCISE
+
+Create two integer variables called "num1" and "num2" and assign them an integer value.
+Print - or just play around with - the various arithmetic operators (i.e. +, -, *, /, %, **, //)
+"""
+
+num1 = 20
+num2 = 3
+
+print(num1 + num2)  # Addition
+print(num1 - num2)  # Subtraction
+print(num1 * num2)  # Multiplication
+print(num1 / num2)  # Division
+print(num1 % num2)  # Modulus
+print(num1 ** num2) # Exponential
+print(num1 // num2) # Floor division
