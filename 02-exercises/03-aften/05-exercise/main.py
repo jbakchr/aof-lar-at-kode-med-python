3e7d681169981749d1d2c8ad7d0e3cb20501eb51
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-05 09:31:11 +0200

    Improved comments

--- 02-exercises/03-aften/05-exercise/main.py
+++ 02-exercises/03-aften/05-exercise/main.py
@@ -1,6 +1,6 @@
 """
 EXERCISE:
 
-Create a variable with a string and print one of it's characters
+Create a variable with a string and print one of it's characters by use of "bracket notation" 
 """
 
