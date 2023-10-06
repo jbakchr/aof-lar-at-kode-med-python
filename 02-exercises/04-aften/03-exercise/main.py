a917a4202a712a346f195f53cb6d343c3cf8aa91
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-06 16:35:20 +0200

    Added exercise

---
+++ 02-exercises/04-aften/03-exercise/main.py
@@ -0,0 +1,8 @@
+"""
+EXERCISE
+
+Steps to complete exercise:
+- 1) Create two integer variables with numbers of your liking
+- 2) Make an if statement evaluate to True using a comparison operator
+- 3) Print something in the body of the if statement
+"""
---
+++ 02-exercises/04-aften/03-exercise/solution/main.py
@@ -0,0 +1,14 @@
+"""
+EXERCISE
+
+Steps to complete exercise:
+- 1) Create two integer variables with numbers of your liking
+- 2) Make an if statement evaluate to True using a comparison operator
+- 3) Print something in the body of the if statement
+"""
+
+num1 = 10
+num2 = 20
+
+if num1 != num2:
+  print("wauw ..")
