b302c051c2b39b45cf8c3a2b8086a4619c7c6f61
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-06 11:38:09 +0200

    Added stuff

--- 02-exercises/04-aften/02-exercise/main.py
+++ 02-exercises/04-aften/02-exercise/main.py
@@ -1,6 +1,6 @@
 """
 EXERCISE
 
-Create two integer variables called "num1" and "num2" and assign them an integer value.
+Create two variables called "num1" and "num2" and assign them an integer value.
 Play around with the various assignment operators and print the result of these assigments
 """
