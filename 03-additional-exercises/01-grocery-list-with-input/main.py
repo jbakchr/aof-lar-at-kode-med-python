897731e6b9e6030c58817ebf8fce547190f890a3
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-06 20:54:13 +0200

    Added additional exercises

---
+++ 03-additional-exercises/01-grocery-list-with-input/main.py
@@ -0,0 +1,12 @@
+"""
+ADDITIONAL EXERCISES
+
+In this exercise you are to:
+- 1)  Create a variable called "groceries" and assign it an empty list
+- 2)  Create a loop that runs 3 times by use of the "range" function
+- 3)  Within the range function loop create a variable that'll be assigned
+      the value from an input function asking the user for something to add
+      to the list variable
+- 4)  Iteratively append the users answer to the list
+- 5)  Print out the list in any way you like
+"""
---
+++ 03-additional-exercises/01-grocery-list-with-input/solution/main.py
@@ -0,0 +1,20 @@
+"""
+SOLUTION TO ADDITIONAL EXERCISES
+
+In this exercise you are to:
+- 1)  Create a variable called "groceries" and assign it an empty list
+- 2)  Create a loop that runs 3 times by use of the "range" function
+- 3)  Within the range function loop create a variable that'll be assigned
+      the value from an input function asking the user for something to add
+      to the list variable
+- 4)  Iteratively append the users answer to the list
+- 5)  Print out the list in any way you like
+"""
+
+groceries = []
+
+for i in range(0, 3):
+  item = input("What do you wanna add to your groceries list? ")
+  groceries.append(item)
+
+print(f"Your groceries list contains: {groceries}")
