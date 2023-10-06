3530d9a251668d150dbe9e3330c2c367c6af8750
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-06 16:25:53 +0200

    Added examples

---
+++ 01-lectures/04-aften/04-comparison-operators/main.py
@@ -0,0 +1,22 @@
+"""
+Examples of comparison operators
+"""
+
+# Example 1 - equals operator
+ex1 = 10 == 10 # True
+ex2 = 10 == 20 # False
+
+
+# Example 2 - not equals operator
+ex3 = 10 != 10 # False
+ex4 = 10 != 20 # True
+
+
+# Example 3 - greater than operator
+ex5 = 10 > 10 # False
+ex6 = 10 > 20 # True
+
+
+# Example - less than operator
+ex7 = 10 < 10 # False
+ex8 = 10 < 20 # True
---
+++ 02-exercises/04-aften/02-exercise-extra-02/main.py
@@ -0,0 +1,17 @@
+"""
+EXTRA EXERCISE
+
+Given that we at least have some experience with looping through lists and text
+you should be able to understand looping by use of Pythons "range" function.
+
+In this exercise your task is to complete the below range loop adding each
+number from each iteration to the "sum" variable above the loop.
+
+And after having completed this you should just print out the sum using Pythons
+"f-string" in any way you'd like.
+"""
+
+sum = 0
+
+for i in range(1, 10):
+  # continiously add and assign the value of "i" to the "sum" variable
