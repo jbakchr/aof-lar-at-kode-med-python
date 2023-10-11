3c3f29a4a30777c22fd79e5b8364740825889515
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-11 12:25:08 +0200

    added stuff

--- 02-exercises/04-aften/08-exercise/solution/main.py
+++ 02-exercises/04-aften/08-exercise/solution/main.py
@@ -1,5 +1,5 @@
 """
-EXERCISE
+SOLUTION TO EXERCISE
 
 In this exercise you first to create a variable called "car" and assign it a dict with the following key / value pairs:
 - 1) "make": "Audi"
@@ -13,3 +13,10 @@ After having done so you should now loop through the "car" dict and do the follo
 
 Lastly just print your car dict to check that it has been updated
 """
+car = {"make": "Audi", "speed": 280, "has_wheels": True, "can_fly": True}
+
+for key in car:
+  if key == "can_fly" and car[key]:
+    car[key] = False
+
+print(car)
