f769cc124a0c8b6296b1fa322c492353e492172b
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-11 16:56:50 +0200

    Added stuff

--- 03-additional-exercises/03-basic-calculator/main.py
+++ 03-additional-exercises/03-basic-calculator/main.py
@@ -1,4 +1,22 @@
-def calculator(num1, op, num2):
+"""
+ADDITIONAL EXERCISE
+
+In this exercise you are to create a function that'll act as a calculator
+when given two numbers and the operator to use.
+
+For now we'll just stick with the "+", "-", "*" and "/" operator
+
+To complete this exercise you should therefor:
+- 1)  Create two variables and assign them the integers you want
+- 2)  Create a variable called "op" and assign it a string of either "+", "-", "*" or "/"
+- 3)  Create a function that should be given 3 parameters named "num1", "num2" and "op"
+- 4)  Make the function use the if, elif and else control structures like this:
+      - 4.1) if the functions "op" parameter is equal to "+" then return the addition of num1 and num2
+      - 4.2) elif the functions "op" parameter is equal to "-" then return the subtraction of num1 and num2
+      - 4.3) elif the functions "op" parameter is equal to "*" then return the subtraction of num1 and num2
+"""
+
+def calculator(num1, num2, op):
   if op == "+":
     return num1 + num2
   elif op == "-":
