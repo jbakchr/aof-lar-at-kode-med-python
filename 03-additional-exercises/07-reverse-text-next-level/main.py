c38f3cda40af8fe5aeae1cf5141b7b7439922c0a
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-12 10:50:13 +0200

    Added exercise and solution

---
+++ 03-additional-exercises/07-reverse-text-next-level/main.py
@@ -0,0 +1,16 @@
+"""
+ADDITIONAL EXERCISE
+
+This exercise kinda builds on top of the previous one - i.e. "06-reverse-text".
+
+Although we haven't gone through it, you are actually able to loop through a
+string backwards when adding a colon (":") and a minus one ("-1") when getting
+a range of a string.
+
+So, if one wanted to reverse a string easily then one would just code the following:
+
+  reverse_name = "Jonas"[::-1]
+
+Given that info, your job now is to create a similar function to that in exercise 06
+that'll reverse a string given to the function when calling it
+"""
