27c296994778fbd239d38ded74a9000733925200
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-12 09:11:33 +0200

    Added solution

--- 03-additional-exercises/06-reverse-text/solution/main.py
+++ 03-additional-exercises/06-reverse-text/solution/main.py
@@ -1,5 +1,5 @@
 """
-ADDITIONAL EXERCISE
+SOLUTION TO ADDITIONAL EXERCISE
 
 In this exercise you are to create a function that takes in some string and
 return the reversed version of it.
@@ -11,3 +11,13 @@ method.
 * HINT * 
 check this link for use of the string "join" method: https://www.w3schools.com/python/ref_string_join.asp
 """
+
+def reverser(txt):
+  reversed_text = []
+
+  for char in txt:
+    reversed_text.insert(0, char)
+  
+  return "".join(reversed_text)
+
+print(reverser("Jonas"))
