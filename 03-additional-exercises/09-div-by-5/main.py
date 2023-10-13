1a2d922ce4bcf603d443732f1cd5b40663bcab33
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-13 09:44:09 +0200

    Added additional exercise 9

---
+++ 03-additional-exercises/09-div-by-5/main.py
@@ -0,0 +1,5 @@
+"""
+ADDITIONAL EXERCISE
+
+Iterate the given list of numbers and print only those numbers which are divisible by 5
+"""
---
+++ 03-additional-exercises/09-div-by-5/solution/main.py
@@ -0,0 +1,11 @@
+"""
+ADDITIONAL EXERCISE
+
+Iterate the given list of numbers and print only those numbers which are divisible by 5
+"""
+
+lst = [10, 20, 33, 46, 55]
+
+for num in lst:
+    if num % 5 == 0:
+        print(num)
