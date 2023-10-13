89e62e4ad89944d6c50d171c72559f477e856586
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-13 11:43:51 +0200

    Fixed description

--- 03-additional-exercises/15-exponent-function/solution/main.py
+++ 03-additional-exercises/15-exponent-function/solution/main.py
@@ -1,5 +1,5 @@
 """
-ADDITIONAL EXERCISE
+SOLUTION TO ADDITIONAL EXERCISE
 
 Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp.
 Note here exp is a non-negative integer, and the base is an integer.
