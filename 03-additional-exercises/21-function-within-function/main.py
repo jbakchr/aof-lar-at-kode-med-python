bfa5c59d3c0e639222e9939ca5c1912a1195db15
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-14 13:45:50 +0200

    Added exercise 21

---
+++ 03-additional-exercises/21-function-within-function/main.py
@@ -0,0 +1,13 @@
+"""
+ADDITIONAL EXERCISE
+
+Create an inner function to calculate the addition in the following way:
+
+Create an outer function that will accept two parameters, a and b.
+
+Create an inner function inside an outer function that will calculate the addition of a and b.
+
+At last, an outer function will add 5 into addition and return it
+"""
+
+
