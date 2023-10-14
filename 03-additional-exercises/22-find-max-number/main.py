60412310ba9d82bc3654ea937c0f1c22b1ce7465
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-14 13:58:20 +0200

    Added exercise

---
+++ 03-additional-exercises/22-find-max-number/main.py
@@ -0,0 +1,10 @@
+"""
+ADDITIONAL EXERCISE
+
+Find the largest item from a given list
+x = [4, 6, 8, 24, 12, 2]
+
+Expected Output:
+24
+"""
+
---
+++ 03-additional-exercises/22-find-max-number/solution/main.py
@@ -0,0 +1,26 @@
+"""
+SOLUTION TO ADDITIONAL EXERCISE
+
+Find the largest item from a given list
+x = [4, 6, 8, 24, 12, 2]
+
+Expected Output:
+24
+"""
+
+# A "looping" solution
+def find_max(lst):
+    largest_num = lst[0]
+    for i in lst[1:]:
+        if i > largest_num:
+            largest_num = i
+    return largest_num
+
+# The quick solution
+def quick_max(lst):
+    return max(lst)
+
+x = [4, 6, 8, 24, 12, 2]
+
+print(find_max(x))
+print(quick_max(x))
