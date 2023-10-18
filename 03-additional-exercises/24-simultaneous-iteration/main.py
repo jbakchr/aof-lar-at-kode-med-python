a3387caf0b0589a4a9e3b77a2d895cd5cfeb622d
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-18 16:33:00 +0200

    Added exercise 24

---
+++ 03-additional-exercises/24-simultaneous-iteration/main.py
@@ -0,0 +1,18 @@
+"""
+ADDITIONAL EXERCISE
+
+Iterate both lists simultaneously
+
+Given a two Python list. Write a program to iterate both lists simultaneously and display items from list1 in original order and items from list2 in reverse order.
+
+Given
+list1 = [10, 20, 30, 40]
+list2 = [100, 200, 300, 400]
+
+Expected output:
+10 400
+20 300
+30 200
+40 100
+"""
+
