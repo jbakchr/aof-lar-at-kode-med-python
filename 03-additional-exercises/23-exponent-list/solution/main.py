a40fd9020f27b212e8c003f9bc96b7d84809d579
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-18 14:36:53 +0200

    Added exercise 23

---
+++ 03-additional-exercises/23-exponent-list/solution/main.py
@@ -0,0 +1,26 @@
+""""
+ADDITIONAL EXERCISE
+
+Turn every item of a list into its square.
+
+Given a list of numbers. write a program to turn every item of a list into its square.
+
+Given:
+numbers = [1, 2, 3, 4, 5, 6, 7]
+
+Expected output:
+[1, 4, 9, 16, 25, 36, 49]
+
+* HINT *
+One solution to this exercise would be to use what's called "list comprehension".
+Try looking up term on Google and found out how one uses it in Python.
+Otherwise, ask your instructor for help.
+"""
+
+
+def square_list(lst: list) -> list:
+    return [num**2 for num in lst]
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7]
+print(square_list(numbers))
