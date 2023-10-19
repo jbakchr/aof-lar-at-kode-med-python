1cd8e6705b361af51e8128c640571f5fef404f11
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-19 11:27:12 +0200

    Added exercise 26

---
+++ 03-additional-exercises/26-combined-list-and-dict/solution/main.py
@@ -0,0 +1,27 @@
+"""
+ADDITIONAL EXERCISE
+
+In this exercise you are to combine and list and a dictionary into another dictionary.
+
+Given this list and dict you are to return the below combined dictionary from a function.
+
+Inputs (list and dict):
+employees = ['Kelly', 'Emma']
+defaults = {"designation": 'Developer', "salary": 8000}
+
+Expected output:
+{'Kelly': {'designation': 'Developer', 'salary': 8000}, 'Emma': {'designation': 'Developer', 'salary': 8000}}
+"""
+
+
+def combined_dict(lst: list, dct: dict) -> dict:
+    computed_dict = {}
+    for el in lst:
+        computed_dict[el] = dct.copy()
+    return computed_dict
+
+
+employees = ["Kelly", "Emma"]
+defaults = {"designation": "Developer", "salary": 8000}
+
+print(combined_dict(employees, defaults))
