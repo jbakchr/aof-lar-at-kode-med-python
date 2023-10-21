345cc0877c06062a4c008cf7fa6a4782ef2d7cdc
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-21 04:46:29 +0200

    Added recap 5

---
+++ 01-lectures/05-aften/01-recap/main.py
@@ -0,0 +1,40 @@
+"""
+Recap from lecture 4
+
+Quick recap of:
+- 1) Operators
+- 2) Data structures
+"""
+
+# RECAP 1 - OPERATORS
+
+# Arithmetic operators (some of them ..)
+ex1 = 20 + 10
+ex2 = 20 - 10
+ex3 = 20 * 10
+ex4 = 20 / 10
+
+
+# Assignment operators (some of them ..)
+ex5 = 10
+
+ex5 += 20   # sum = 30
+ex5 -= 10   # sum = 20
+ex5 *= 2    # sum = 40
+ex5 /= 4    # sum = 10
+
+
+# Comparison operators (==, !=, >, <, >=, <=)
+ex6 = 4 == 4  # True
+ex7 = 4 != 4  # False
+ex8 = 5 > 4   # True
+ex9 = 5 < 4   # False
+
+
+# Logical operators ("and", "or", "not")
+ex10 = 4 == 4 and 4 != 5  # True
+ex11 = 4 == 4 or 4 != 4   # True
+ex11 = not 4 == 4         # False
+
+
+# RECAP 2 - DATA STRUCTURES
