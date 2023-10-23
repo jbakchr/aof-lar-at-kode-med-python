c5967797b680c0001b79d86a7e2a95c2cc3b2e99
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-23 21:17:48 +0200

    Added for loops

---
+++ 01-lectures/05-aften/03-for-loops/main.py
@@ -0,0 +1,20 @@
+"""
+Examples of for loops
+"""
+
+# EXAMPLE 1 - RANGE LOOPS
+
+# range with only "stop" value
+for i in range(11):
+  print(f"Num is: {i}")
+
+# range with "start" and "stop" value
+for i in range(1, 11):
+  print(f"Num is: {i}")
+
+# range with "start", "stop" and "step" value
+for i in range(1, 11, 2):
+  print(f"Num is: {i}")
+
+
+# EXAMPLE 2 - FOR LOOPS
