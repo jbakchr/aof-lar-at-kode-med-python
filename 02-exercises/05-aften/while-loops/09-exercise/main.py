1b815b43240685492c3e7b4a2272f5f38ff8664d
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-24 09:07:36 +0200

    Added exercise 9

---
+++ 02-exercises/05-aften/while-loops/09-exercise/main.py
@@ -0,0 +1,6 @@
+"""
+Exercise 9
+
+Create a while loop that'll print the numbers from 1 to 10
+"""
+
---
+++ 02-exercises/05-aften/while-loops/09-exercise/solution/main.py
@@ -0,0 +1,9 @@
+"""
+Exercise 9
+
+Create a while loop that'll print the numbers from 1 to 10
+"""
+num = 1
+while num <= 10:
+  print(num)
+  num += 1
