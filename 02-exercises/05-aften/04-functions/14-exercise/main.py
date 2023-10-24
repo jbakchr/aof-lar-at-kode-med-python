2e6ce735a12f7478791d4ae09ff2e9ce7bf0c51d
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-24 12:02:07 +0200

    Added exercise 14

---
+++ 02-exercises/05-aften/04-functions/14-exercise/main.py
@@ -0,0 +1,6 @@
+"""
+Exercise 14
+
+Create a function that takes a number as a paratemer, raises that number to the
+power of 2 and prints it.
+"""
---
+++ 02-exercises/05-aften/04-functions/14-exercise/solution/main.py
@@ -0,0 +1,8 @@
+"""
+Exercise 14
+
+Create a function that takes a number as a paratemer, raises that number to the
+power of 2 and prints it.
+"""
+def exponent(num):
+  print(num ** 2)
