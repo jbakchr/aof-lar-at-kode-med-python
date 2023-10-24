02a4c746178afc8a7317180f12ccb3afd40e8f44
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-24 11:09:08 +0200

    Added examples of simple functions

---
+++ 01-lectures/05-aften/05-simple-functions/main.py
@@ -0,0 +1,21 @@
+"""
+Examples of functions
+"""
+
+# Example 1 - defining a function without parameters
+def my_function():
+  print("This is a super function!")
+
+
+# Example 2 - Calling a function
+my_function()
+
+
+# Example 3 - defining a function with parameters
+def multiply(num1, num2):
+  print(num1 * num2)
+
+
+# Example 4 - function that returns some data
+def greet_me(name):
+  return f"Hi, {name}"
