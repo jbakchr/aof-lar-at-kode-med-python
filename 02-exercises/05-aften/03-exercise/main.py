a23fe9ac8be9d1af10631c40313a1c2294a20993
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-24 06:38:58 +0200

    Added exercise 3

---
+++ 02-exercises/05-aften/03-exercise/main.py
@@ -0,0 +1,5 @@
+"""
+Exercise 3
+
+Create a loop using the range function that prints the odd numbers from 1 to 10
+"""
---
+++ 02-exercises/05-aften/03-exercise/solution/main.py
@@ -0,0 +1,7 @@
+"""
+Solution to exercise 3
+
+Create a loop using the range function that prints the odd numbers from 1 to 10
+"""
+for i in range(1, 10, 2):
+  print(i)
