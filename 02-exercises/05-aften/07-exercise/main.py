5098cd4e951ef9d48f4e25b929ec98c8dfe9e213
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-24 07:36:03 +0200

    Added exercise 8

---
+++ 02-exercises/05-aften/07-exercise/main.py
@@ -0,0 +1,6 @@
+"""
+Exercise 7
+
+Just loop through a range of numbers from 1 to 5 and print something in an else
+block when the loop finishes
+"""
---
+++ 02-exercises/05-aften/07-exercise/solution/main.py
@@ -0,0 +1,11 @@
+"""
+Exercise 7
+
+Just loop through a range of numbers from 1 to 5 and print something in an else
+block when the loop finishes
+"""
+
+for i in range(1, 6):
+  print(i)
+else:
+  print("I'm done!")
