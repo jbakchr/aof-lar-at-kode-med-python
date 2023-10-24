5659bf487c302aaca523d16aeac62ac0bb8a5f45
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-24 12:02:42 +0200

    Changed exercise 14

--- 02-exercises/05-aften/04-functions/14-exercise/solution/main.py
+++ 02-exercises/05-aften/04-functions/14-exercise/solution/main.py
@@ -2,7 +2,7 @@
 Exercise 14
 
 Create a function that takes a number as a paratemer, raises that number to the
-power of 2 and prints it.
+power of 2 and returns it
 """
 def exponent(num):
-  print(num ** 2)
+  return num ** 2
