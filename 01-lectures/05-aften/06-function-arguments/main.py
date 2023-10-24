aaf4d1186a40cc5336cfbe7af2ee608d2e8eee26
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-24 12:48:34 +0200

    Added examples

---
+++ 01-lectures/05-aften/06-function-arguments/main.py
@@ -0,0 +1,31 @@
+"""
+Examples of function arguments
+"""
+
+# Example 1 - function with many arguments
+def say_hello(first_name, last_name, age, city):
+  print(f"Hi, my name is {first_name} {last_name} and I'm {age} years old and live in {city}")
+
+
+# Example 2 - function with arbitary number of arguments
+names = ["John", "Mogens", "Palle", "Jørgen", "Djarnis"]
+
+def call_out(*args):
+  for name in args:
+    print(name)
+
+
+# Example 3 - function with keyword arguments
+def call_out_my_kids(first_child, second_child):
+  print(f"My youngest kid is: {second_child}")
+
+call_out_my_kids(second_child="Edgar", first_child="Milton")
+
+
+# Example 4 - function with arbitary number of keyword arguments
+def my_family(**kwargs):
+  print("My nearest family is:")
+  for k, v in kwargs.items():
+    print(f"{k}: {v}")
+
+my_family(mom="Susanne", dad="Bent", brother="Christian")
